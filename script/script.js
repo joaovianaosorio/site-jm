c3dda147dd3e53af11eee14000d976968c555588
João Vitor Osório <joaovianaosorio@gmail.com>
2026-08-06 21:07:36 -0300

    atualizações
    script do menu sidebar feito
    mudanças no design dos cards de próteses

--- script/script.js
+++ script/script.js
@@ -1,3 +1,20 @@
+// MENU
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+const sidebar = document.getElementById('sidebar');
+const overlay = document.getElementById('overlay');
+
+function toggleMenu() {
+  sidebar.classList.toggle('active');
+  overlay.classList.toggle('active');
+}
+
+menuBtn.addEventListener('click', toggleMenu);
+closeBtn.addEventListener('click', toggleMenu);
+overlay.addEventListener('click', toggleMenu);
+
+// CARROSSEL
+
 // 1. Seleciona os elementos no HTML
 const carrossel = document.getElementById('carrossel');
 const btnPrev = document.getElementById('btn-prev');
