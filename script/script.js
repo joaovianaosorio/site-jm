e63383fadb09039765bffe2db1380a5c1119d23a
João Vitor Osório <joaovianaosorio@gmail.com>
2026-08-15 21:09:36 -0300

    atualizações
    atualizei carrossel para os tipos de protese
    criei seção de tecnologias (será usada para falar de máquinas 3D)
    criei seção de procedimentos usados para adaptação de prótese
    criação do efeito wave

--- script/script.js
+++ script/script.js
@@ -13,6 +13,36 @@ menuBtn.addEventListener('click', toggleMenu);
 closeBtn.addEventListener('click', toggleMenu);
 overlay.addEventListener('click', toggleMenu);
 
+// LÓGICA DE FILTRAGEM POR ABAS
+const tabBtns = document.querySelectorAll('.tab-btn');
+const trabalhoCards = document.querySelectorAll('.trabalho-card');
+
+tabBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // Remove a classe 'active' de todos os botões e adiciona no clicado
+    tabBtns.forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+
+    const categoriaSelecionada = btn.getAttribute('data-categoria');
+
+    // Filtra os cards
+    trabalhoCards.forEach(card => {
+    // Transforma a lista de categorias em um array de palavras
+    const categoriasDoCard = card.getAttribute('data-categoria').split(' ');
+
+    // Verifica se a categoria clicada está na lista do card
+    if (categoriasDoCard.includes(categoriaSelecionada)) {
+      card.classList.remove('escondido');
+    } else {
+      card.classList.add('escondido');
+    }
+    });
+
+    // Reseta a rolagem do carrossel para o início ao trocar de aba
+    carrossel.scrollTo({ left: 0, behavior: 'smooth' });
+  });
+});
+
 // CARROSSEL
 
 // 1. Seleciona os elementos no HTML
@@ -36,5 +66,13 @@ btnPrev.addEventListener('click', () => {
   carrossel.scrollBy({
     left: -scrollAmount,
     behavior: 'smooth'
+  });
+});
+
+// ANTES E DEPOIS
+
+document.querySelectorAll('.card-antes-depois').forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('virado');
   });
 });
